e3ca1d3cdab8fb86d8962c16df9ec57289f8f6f4
Abhishek khutal <157826878+khutal-abhishek@users.noreply.github.com>
2024-08-27 18:23:30 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+//"use strict" //treat all JS code as newer Version
+//alert(3+3) // we are using nodeJS not broweser so it is not runing on here....
+console.log(3
+    +3); // code Readability must be high.
+console.log("Abhii");
+
+let name="Abhii"
+let age=21
+let isLoggedIn=false;
+let state;
+
+//number => 2 to power of 53
+//bigint =>
+//string => "",''
+//boolean => true/false
+//null => standalone value/Empty value
+//undefined =>
+//symbol => Unique
+
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null); //object

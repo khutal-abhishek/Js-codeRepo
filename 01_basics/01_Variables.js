1b8e231087f3dbd47439700f41d32523bb2a8336
Abhishek khutal <157826878+khutal-abhishek@users.noreply.github.com>
2024-08-27 17:57:51 +0000

    Changes in Variables File

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,16 @@
+const accountId=232931
+let accountEmail="ab@gmail.com"
+var accountPassword="232134"
+accountCity="Pune"
+
+let accountState;
+// accountId=234567; //we cannot modify the variable we define using  Const keyword...
+
+accountEmail="abhi@gmail.com"
+accountPassword="87652"
+accountCity="Mumbai"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountState,accountCity])
+
